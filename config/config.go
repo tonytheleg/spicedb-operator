672644946879e07754b4f17013c9bb056e1b767e
Antony Natale <anatale@redhat.com>
2023-10-30 14:11:22 -0400

    more updates for boilerplate

---
+++ config/config.go
@@ -0,0 +1,6 @@
+package config
+
+const (
+	OperatorName      string = "spicedb-operator"
+	OperatorNamespace string = "spicedb-operator"
+)
--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/authzed/spicedb-operator
 
-go 1.19
+go 1.20
 
 require (
 	github.com/authzed/controller-idioms v0.9.1
